bc48e99373b1e31333c5b22c45bfbebc21902bc6
denysbaranchykov <denysbaranchykov@gmail.com>
2023-05-20 10:47:23 +0200

    fixed with 2 spans and position 'do not watch the logic for this button, it is not ready'

--- src/components/ColumnComponent.tsx
+++ src/components/ColumnComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './ColumnComponent.module.css';
 
 interface columnComponentProps {
@@ -6,19 +6,38 @@ interface columnComponentProps {
 }
 
 const ColumnComponent = ({title}: columnComponentProps) => {
+    const [clicked, setClicked] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+    const clickAddCard = () => {
+        setClicked(prev => !prev)
+    }
+    const changeValue = (e: any) => {
+        setInputValue(e.target.value)
+    }
+
+    console.log('value', inputValue);
+
     return (
         <>
             <h1>{title}</h1>
             <div className={s.wrapper}>
                 <h2 className={s.header}>To Do</h2>
                 <div className={s.buttonForm}
-                     onClick={() => console.log('Add card clicked')}
+                     onClick={clickAddCard}
                 >
-                    <span className={s.buttonText}>+ Add card</span>
+                    <span className={s.buttonPlus}>+</span>
+                    <span className={s.buttonText}>Add card</span>
                 </div>
+                {
+                    clicked && <input
+                        type="text"
+                        value={inputValue}
+                        onChange={changeValue}
+                    />
+                }
             </div>
+            {console.log('clicked', clicked)}
         </>
-
     );
 };
 
