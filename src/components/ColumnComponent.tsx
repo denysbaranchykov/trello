4fbfb0a5b30ee7c7c38247e3d4a96c870591beb1
denysbaranchykov <denysbaranchykov@gmail.com>
2023-05-27 12:29:13 +0200

    add useRef for input focus but not work

--- src/components/ColumnComponent.tsx
+++ src/components/ColumnComponent.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import s from './ColumnComponent.module.css';
 
 interface columnComponentProps {
@@ -6,37 +6,46 @@ interface columnComponentProps {
 }
 
 const ColumnComponent = ({title}: columnComponentProps) => {
-    const [clicked, setClicked] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
     const [inputValue, setInputValue] = useState('');
     const clickAddCard = () => {
-        setClicked(prev => !prev)
+        setIsClicked(prev => !prev)
     }
     const changeValue = (e: any) => {
         setInputValue(e.target.value)
     }
-
-    console.log('value', inputValue);
+const inputFocus: any =  useRef(null);
+    if (inputFocus.current !== null) {
+            inputFocus.current.focus()
+    }
 
     return (
         <>
             <h1>{title}</h1>
+            <p>{inputValue}</p>
             <div className={s.wrapper}>
                 <h2 className={s.header}>To Do</h2>
+                {
+                    isClicked &&
+                    <span onClick={() => console.log('cancel')}>Cancel</span>
+                }
                 <div className={s.buttonForm}
                      onClick={clickAddCard}
                 >
-                    <span className={s.buttonPlus}>+</span>
-                    <span className={s.buttonText}>Add card</span>
+                    {isClicked ? <input className={s.inputField}
+                                        ref={inputFocus}
+                                        type="text"
+                                        value={inputValue}
+                                        onChange={changeValue}
+                        />
+                        :
+                        <div className={s.addCard}>
+                            <span className={s.buttonPlus}>+</span>
+                            <span className={s.buttonText}>Add card</span>
+                        </div>
+                    }
                 </div>
-                {
-                    clicked && <input
-                        type="text"
-                        value={inputValue}
-                        onChange={changeValue}
-                    />
-                }
             </div>
-            {console.log('clicked', clicked)}
         </>
     );
 };
