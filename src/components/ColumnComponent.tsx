be353a74a31570df2df24a4418ad1461f2fc319e
denysbaranchykov <denysbaranchykov@gmail.com>
2023-07-03 15:55:23 +0200

    inputRef in processing

--- src/components/ColumnComponent.tsx
+++ src/components/ColumnComponent.tsx
@@ -1,31 +1,83 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import s from './ColumnComponent.module.css';
-import {isDisabled} from "@testing-library/user-event/dist/utils";
 
 interface columnComponentProps {
     title: string
 }
 
+interface myCardsType {
+    title: string,
+    id: string
+}
+
 const ColumnComponent = ({title}: columnComponentProps) => {
     const [isClicked, setIsClicked] = useState(false);
-    const [inputValue, setInputValue] = useState('');
-    const changeValue = (e: any) => {
-        setInputValue(e.target.value);
-    };
+    // const [inputValue, setInputValue] = useState('');
+    const [cards, setCards] = useState<myCardsType[]>([
+        {title: 'card1', id: '1'},
+        {title: 'card2', id: '2'},
+    ]);
     const clickAddCard = () => {
         setIsClicked(prev => !prev);
     };
     const cancelClick = () => {
         setIsClicked(false)
     };
+    const inputRef = useRef<HTMLInputElement>(null);
     const addClick = () => {
-       console.log('add');
+        if(inputRef.current?.value) {
+            setCards((prev) => [
+                    ...prev,
+                    {
+                        title: inputRef.current?.value as string,
+                        id: generateId()
+                    }
+                ]
+            );
+        }
     };
+    console.log('cards', cards);
+
+
+
+
+
+    let uniqueId = new Set();
+    console.log('uniqueId', uniqueId);
+
+    const generateId = (): string => {
+        let id = '';
+        let digits = '0123456789';
+        for (let i = 0; i < 10; i++) {
+            id += Math.floor(Math.random() * digits.length);
+        }
+
+        //  2 way
+        // let alphabetAndDigits = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        // for (let i = 0; i < 10; i++) {
+        //     id += alphabetAndDigits.charAt(Math.floor(Math.random() * alphabetAndDigits.length));
+        // }
+
+
+        // 3 way
+        // let result = Math.random().toString(36).substring(2,13);
+        // return result;
+
+
+        //check if already exist but have problems here with empty new Set
+        if (uniqueId.has(id)) {
+            return generateId()
+        }
+
+        uniqueId.add(id);
+        return id;
+    }
+    console.log('current value', inputRef.current?.value.replace(/\s/g, '').length === 1);
 
     return (
         <>
             <h1>{title}</h1>
-            <p>{inputValue}</p>
+            <p>{inputRef.current?.value}</p>
             <div className={s.wrapper}>
                 <h2 className={s.header}>To Do</h2>
                 {
@@ -33,17 +85,17 @@ const ColumnComponent = ({title}: columnComponentProps) => {
                     <div>
                         <input className={s.inputField}
                                autoFocus={true}
+                               ref={inputRef}
                                type="text"
-                               value={inputValue}
-                               onChange={changeValue}
                         />
                         <button className={s.cancel}
-                              onClick={cancelClick}>Cancel</button>
+                                onClick={cancelClick}>Cancel
+                        </button>
                         <button className={s.add}
-                              disabled={!inputValue || inputValue.replace(/\s/g, '').length === 0}
-                              onClick={addClick}>Add</button>
+                                disabled={inputRef.current?.value.replace(/\s/g, '').length === 0}
+                                onClick={addClick}>Add
+                        </button>
                     </div>
-
                 }
                 <div className={s.buttonForm}
                      onClick={clickAddCard}
